2177b159ebc1b0ab269bdca92e9fe4e3e10a53ad
syntax2 <akadian135@gmail.com>
2025-07-04 17:54:30 +0530

    NAT and route table association

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -75,3 +75,40 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 
 }
+
+resource "aws_nat_gateway" "main" {
+    count = length(var.public_subnet_cidrs)
+    allocation_id = aws_eip.nat.id
+    subnet_id     = aws_subnet.public[0].id
+    
+    tags = {
+        Name = "${var.cluster_name}-nat-gateway"
+        "Kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    }
+  
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  count = length(var.private_subnet_cidrs)
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id        
+
+  } 
+
+  tags = {
+    Name = "${var.cluster_name}-private-route-table-${count.index + 1}"
+    "Kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  } 
+}
+
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+  
+}
