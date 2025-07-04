09258cfe0fca2662642ac4d4cbd5033cb3a9cfe4
syntax2 <akadian135@gmail.com>
2025-07-04 18:21:16 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -68,3 +68,27 @@ resource "aws_iam_role_policy_attachment" "node_policy" {
   policy_arn = each.value
 
 }
+
+
+resource "aws_eks_node_group" "main" {
+    for_each = var.node_groups
+    
+    cluster_name    = var.cluster_name
+    node_group_name = "${var.cluster_name}-${each.key}-node-group"
+    node_role_arn   = aws_iam_role.node.arn
+    subnet_ids      = var.subnet_ids
+    
+    scaling_config {
+        desired_size = each.value.scaling_config.desired_size
+        max_size     = each.value.scaling_config.max_size
+        min_size     = each.value.scaling_config.min_size
+    }
+    
+    instance_types = [each.value.instance_type]
+    capacity_type = each.value.capacity_type
+     
+    depends_on = [
+        aws_iam_role_policy_attachment.node_policy
+    ]
+  
+}
