88be51c8ad5721c734ceec0f3eca935448173042
syntax2 <akadian135@gmail.com>
2025-07-04 17:41:35 +0530

    adding subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,3 +8,19 @@ resource "aws_vpc" "main" {
     "Kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 }
+
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "${var.cluster_name}-private-subnet-${count.index + 1}"
+    "Kubernetes.io/role/internal-elb" = "1"
+    "Kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+  
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -9,3 +9,22 @@ variable "availability_zones" {
   type        = list(string)
   
 }
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = ""
+  
+}
