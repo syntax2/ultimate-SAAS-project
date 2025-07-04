15b014c9d2741e1e230f30c8e5671c6887d13289
syntax2 <akadian135@gmail.com>
2025-07-04 17:47:17 +0530

    IGW and route table association

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -24,3 +24,54 @@ resource "aws_subnet" "private" {
   }
   
 }
+
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.cluster_name}-public-subnet-${count.index + 1}"
+    "Kubernetes.io/role/elb" = "1"
+    "Kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+  
+}
+
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.cluster_name}-igw"
+    "Kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+  
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-public-route-table"
+    "Kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+
+}
